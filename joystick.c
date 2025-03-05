4470fa2b3af9d007397c0e5321512d912585f42d
Adam <adam.kuchto@ctm.gdynia.pl>
2025-03-05 12:29:25 +0100

    change mapping

--- joystick.c
+++ joystick.c
@@ -18,30 +18,30 @@ int setY(int value)
 struct Point updateDot(int x, int y)
 {
 	struct Point point;
+//TODO Dodać tworzenie tej zależności automatycznie.
+	if (x == -5) point.x = 1;
+	if (x == -4) point.x = 2;
+	if (x == -3) point.x = 3;
+	if (x == -2) point.x = 4;
+	if (x == -1) point.x = 5;
+	if (x == 0) point.x = 6;
+	if (x == 1) point.x = 7;
+	if (x == 2) point.x = 8;
+	if (x == 3) point.x = 9;
+	if (x == 4) point.x = 10;
+	if (x == 5) point.x = 11;
 
-	if (x == -5) point.x = 0;
-	if (x == -4) point.x = 1;
-	if (x == -3) point.x = 2;
-	if (x == -2) point.x = 3;
-	if (x == -1) point.x = 4;
-	if (x == 0) point.x = 5;
-	if (x == 1) point.x = 6;
-	if (x == 2) point.x = 7;
-	if (x == 3) point.x = 8;
-	if (x == 4) point.x = 9;
-	if (x == 5) point.x = 10;
-
-	if (y == -5) point.y = 10;
-	if (y == -4) point.y = 9;
-	if (y == -3) point.y = 8;
-	if (y == -2) point.y = 7;
-	if (y == -1) point.y = 6;
-	if (y == 0) point.y = 5;
-	if (y == 1) point.y = 4;
-	if (y == 2) point.y = 3;
-	if (y == 3) point.y = 2;
-	if (y == 4) point.y = 1;
-	if (y == 5) point.y = 0;
+	if (y == -5) point.y = 11;
+	if (y == -4) point.y = 10;
+	if (y == -3) point.y = 9;
+	if (y == -2) point.y = 8;
+	if (y == -1) point.y = 7;
+	if (y == 0) point.y = 6;
+	if (y == 1) point.y = 5;
+	if (y == 2) point.y = 4;
+	if (y == 3) point.y = 3;
+	if (y == 4) point.y = 2;
+	if (y == 5) point.y = 1;
 
 	return point;
 }
